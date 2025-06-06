305ce08fb5a686cd6c3106a67bf2d3a9e1a0d841
marianegri <mariamnegri@gmail.com>
2025-06-06 09:28:09 -0700

    Added pswd to .env file

---
+++ wait-for-db.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until nc -z -v -w30 "$host"; do
+  echo "Waiting for $host to be ready..."
+  sleep 1
+done
+
+echo "$host is up - executing command"
+exec $cmd
